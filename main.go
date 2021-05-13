43e077a1cc42920059c8040b1704b2f62a56f8f6
Juandi2200 <57282878+Juandi22001@users.noreply.github.com>
2021-05-12 22:13:08 -0600

    Add files via upload

--- main.go
+++ main.go
@@ -9,7 +9,8 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
-
+      "os"
+	  "io"
 	_ "github.com/godror/godror"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
@@ -189,11 +190,122 @@ type Res_Login struct {
 Con una peticion POST DE REACT se llenara
 
 */
+
+func EditarDeportes(w http.ResponseWriter, r *http.Request) {
+
+	var pruebita Deporte
+
+	reqBody, _ := ioutil.ReadAll(r.Body)
+
+	json.Unmarshal(reqBody, &pruebita)
+	println("se esta creando un deporte de nombre--" + pruebita.Nombre_Deportes)
+
+	println("color de deporte---" + pruebita.Color_Deporte)
+
+	println("FOto  de deporte---" + pruebita.Imagen_Deporte)
+
+	db, err := sql.Open("godror", "mia1/mia1@localhost:1521/ORCLCDB.localdomain")
+	//Oracle 18c
+	// db, err := sql.Open("godror", "user/password@localhost:1521/ORCL18.localdomain")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer db.Close()
+
+	//var prueba =pruebita.Color_Deporte;
+	//var prueba2=pruebita.Imagen_Deporte
+	base = db
+	json.Unmarshal(reqBody, &pruebita)
+	/*querry_cargar_deportes := `INSERT INTO DEPORTE (nombre_deporte,color_deporte,imagen_deoirte)  VALUES( '`+ pruebita.Nombre_Deportes +  `'`+ `,`+`'`+pruebita.Color_Deportes+`'`+
+	`,`+`'`+pruebita.Imagen_Deporte+`'`+`);`
+	*/
+
+	res, err := db.Exec("BEGIN procedimiento_EDITARDEPORTE(:1, :2, :3);end;", pruebita.Nombre_Deportes, pruebita.Color_Deporte, pruebita.Imagen_Deporte)
+
+	fmt.Print(res)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.WriteHeader(http.StatusOK)
+
+	json.NewEncoder(w).Encode(pruebita)
+
+}
+
+
+
+func EliminarDeportes(w http.ResponseWriter, r *http.Request) {
+
+	var pruebita Deporte
+
+	reqBody, _ := ioutil.ReadAll(r.Body)
+
+	json.Unmarshal(reqBody, &pruebita)
+	println("se esta creando un deporte de nombre--" + pruebita.Nombre_Deportes)
+
+	println("color de deporte---" + pruebita.Color_Deporte)
+
+	println("FOto  de deporte---" + pruebita.Imagen_Deporte)
+	vars := mux.Vars(r)
+
+	deporte := vars["deporte"]
+
+	r.ParseForm()
+
+	db, err := sql.Open("godror", "mia1/mia1@localhost:1521/ORCLCDB.localdomain")
+	//Oracle 18c
+	// db, err := sql.Open("godror", "user/password@localhost:1521/ORCL18.localdomain")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer db.Close()
+
+	//var prueba =pruebita.Color_Deporte;
+	//var prueba2=pruebita.Imagen_Deporte
+	base = db
+	json.Unmarshal(reqBody, &pruebita)
+	/*querry_cargar_deportes := `INSERT INTO DEPORTE (nombre_deporte,color_deporte,imagen_deoirte)  VALUES( '`+ pruebita.Nombre_Deportes +  `'`+ `,`+`'`+pruebita.Color_Deportes+`'`+
+	`,`+`'`+pruebita.Imagen_Deporte+`'`+`);`
+	*/
+
+	res, err := db.Exec("BEGIN procedimiento_ELIMINARDEPORTE(:1);end;", deporte)
+
+	fmt.Print(res)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.WriteHeader(http.StatusOK)
+
+	json.NewEncoder(w).Encode(pruebita)
+
+}
+
+
+
+
+
+
 func CrearDeportes(w http.ResponseWriter, r *http.Request) {
 
 	var pruebita Deporte
 
 	reqBody, _ := ioutil.ReadAll(r.Body)
+	pruebita.Imagen_Deporte = pruebita.Imagen_Deporte + ".jpg"
+	foto = pruebita.Imagen_Deporte
 	fmt.Print(pruebita)
 	json.Unmarshal(reqBody, &pruebita)
 	println("se esta creando un deporte de nombre--" + pruebita.Nombre_Deportes)
@@ -228,13 +340,6 @@ func CrearDeportes(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	/*	res1 := strings.SplitAfter(pruebita.Imagen_Deporte, "fakepath")
-		     fmt.Print(res1)
-
-		    var FotoDeporte string;
-			FotoDeporte="C:"+"/path";
-		 fmt.Print(FotoDeporte)
-	*/
 
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
@@ -400,11 +505,8 @@ func insert_CargaMasivaModelo(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(pruebita)
 
 }
+func editar_Clientes(w http.ResponseWriter, r *http.Request) {
 
-
-func insert_Clientes(w http.ResponseWriter, r *http.Request) {
-
-	fmt.Print(r)
 	var pruebita USUARIO
 	//var loginprueba login
 	// we will need to extract the `id` of the article we
@@ -424,12 +526,127 @@ func insert_Clientes(w http.ResponseWriter, r *http.Request) {
 	defer db.Close()
 
 
+  
+
+
+	base = db
+	json.Unmarshal(reqBody, &pruebita) 
+	res, err := db.Exec("BEGIN PROCEDIMIENTO_EDITARUSUARIO(:1, :2, :3,:4,:5);end;", pruebita.USERNAME,pruebita.PASSWORD,pruebita.NOMBRE_USUARIO,pruebita.APELLIDO_USUARIO,pruebita.FOTOPERFIL)
+
+
+	fmt.Print("suuuuuuuuuuuuu")
+	fmt.Print(res)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.WriteHeader(http.StatusOK)
+   fmt.Print(pruebita.USERNAME)
+	json.NewEncoder(w).Encode(pruebita)
+
+}
+
+
+
+func Recuperar(w http.ResponseWriter, r *http.Request) {
+
+	var pruebita USUARIO
+	//var loginprueba login
+	// we will need to extract the `id` of the article we
+	// wish to delete
+	reqBody, _ := ioutil.ReadAll(r.Body)
+
+	//---el body lo vuelvo un struct para acceder a sus atributos
+
+	db, err := sql.Open("godror", "mia1/mia1@localhost:1521/ORCLCDB.localdomain")
+	//Oracle 18c
+	// db, err := sql.Open("godror", "user/password@localhost:1521/ORCL18.localdomain")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer db.Close()
+
+
+  
+
+
+	base = db
+	json.Unmarshal(reqBody, &pruebita) 
+	res, err := db.Exec("BEGIN  CAMBIAR_password(:1 );end;", pruebita.USERNAME)
+
+
+	fmt.Print("suuuuuuuuuuuuu")
+	fmt.Print(res)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.WriteHeader(http.StatusOK)
+   fmt.Print(pruebita.USERNAME)
+	json.NewEncoder(w).Encode(pruebita)
+
+}
+var foto string;
+func uploader(w http.ResponseWriter, r *http.Request) {
+	r.ParseMultipartForm(4000)
+	file, fileInfo, err := r.FormFile("file")
+	if err != nil {
+		log.Fatal(err)
+		json.NewEncoder(w).Encode(1)
+		return
+	}
+	print(fileInfo.Filename)
+	println(foto)
+	f, err := os.OpenFile("../public/"+foto, os.O_WRONLY|os.O_CREATE, 0777)
+	if err != nil {
+	
+		log.Fatal(err)
+		json.NewEncoder(w).Encode(1)
+		return
+	}
+	defer f.Close()
+	io.Copy(f, file)
+	fmt.Print(fileInfo)
+	json.NewEncoder(w).Encode(0)
+}
+
+func insert_Clientes(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Print(r)
+	var pruebita USUARIO
+
+	//var loginprueba login
+	// we will need to extract the `id` of the article we
+	// wish to delete
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(reqBody, &pruebita)
+	pruebita.FOTOPERFIL = pruebita.USERNAME + ".jpg"
+	foto = pruebita.FOTOPERFIL
+	//---el body lo vuelvo un struct para acceder a sus atributos
+
+	db, err := sql.Open("godror", "mia1/mia1@localhost:1521/ORCLCDB.localdomain")
+	//Oracle 18c
+	// db, err := sql.Open("godror", "user/password@localhost:1521/ORCL18.localdomain")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer db.Close()
+
+
     fmt.Print("-----"+pruebita.FECHA_REGISTRO)
 
-    fmt.Print("--*-*-*-+"+pruebita.FECHA_NACIMIENTO)
+
 	base = db
-	json.Unmarshal(reqBody, &pruebita)
-	res, err := db.Exec("BEGIN procedimiento_INSERTUSUARIO(:1, :2, :3,:4,:5,:6,:7);end;", pruebita.USERNAME,pruebita.PASSWORD,pruebita.NOMBRE_USUARIO,pruebita.APELLIDO_USUARIO,pruebita.FECHA_NACIMIENTO,pruebita.FECHA_REGISTRO,pruebita.FOTOPERFIL)
+
+	res, err := db.Exec("BEGIN procedimiento_INSERTUSUARIO(:1, :2, :3,:4,:5,:6);end;", pruebita.USERNAME,pruebita.PASSWORD,pruebita.NOMBRE_USUARIO,pruebita.APELLIDO_USUARIO,pruebita.FECHA_NACIMIENTO,pruebita.FOTOPERFIL)
 
 
 	fmt.Print("suuuuuuuuuuuuu")
@@ -480,46 +697,42 @@ func insert_USERS(w http.ResponseWriter, r *http.Request) {
 
 func Perfil(w http.ResponseWriter, r *http.Request) {
 
-	
-	fmt.Print(r)
-	var pruebita CargaMasiva
-	//var loginprueba login
-	// we will need to extract the `id` of the article we
-	// wish to delete
-	reqBody, _ := ioutil.ReadAll(r.Body)
 
-	//---el body lo vuelvo un struct para acceder a sus atributos
+	vars := mux.Vars(r)
 
-	db, err := sql.Open("godror", "mia1/mia1@localhost:1521/ORCLCDB.localdomain")
-	//Oracle 18c
-	// db, err := sql.Open("godror", "user/password@localhost:1521/ORCL18.localdomain")
-	if err != nil {
-		fmt.Println(err)
-		return
+	nickname := vars["user"]
+
+	r.ParseForm()
+
+	limit, _ := strconv.Atoi(r.FormValue("limit"))
+
+	clientes := GetUSER(limit)
+
+	for _, cliente := range clientes {
+
+		if cliente.USERNAME == nickname {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(cliente)
+		}
+
 	}
-
-	defer db.Close()
-
-	base = db
-	json.Unmarshal(reqBody, &pruebita)
-	
-	res, err := db.Exec("BEGIN MODELO;end;" )
-
-
-
-	fmt.Print(res)
-	fmt.Print(res)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.WriteHeader(http.StatusOK)
-
-	json.NewEncoder(w).Encode(pruebita)
 
 }
+
+func GetDeportes(w http.ResponseWriter, r *http.Request) {
+
+
+
+	r.ParseForm()
+
+	limit, _ := strconv.Atoi(r.FormValue("limit"))
+
+	clientes := GetDeporte(limit)
+	json.NewEncoder(w).Encode(clientes)
+	
+
+}
+
 
 
 func  Perfiles(w http.ResponseWriter, r *http.Request) {
@@ -589,7 +802,8 @@ carga masiva deporteeeeeee
 
 */
 
-func GetPeople(n int, USERNAME string ) []USUARIO {
+
+func GetUSER(n int) []USUARIO {
 	people := make([]USUARIO, 0)
 
 	db, err := sql.Open("godror", "mia1/mia1@localhost:1521/ORCLCDB.localdomain")
@@ -597,19 +811,34 @@ func GetPeople(n int, USERNAME string ) []USUARIO {
 		fmt.Println(err)
 		
 	}
-
 	defer db.Close()
-	
 	base = db
-	rows, _ := db.Query(`SELECT  USERNAME,NOMBRE_USUARIO,APELLIDO_USUARIO,FECHA_NACIMIENTO,FECHA_REGISTRO	 from USUARIO WHERE USERNAME`+`=`+`'`+USERNAME+`'`, n)
+	rows, _ := db.Query(`SELECT  USERNAME,NOMBRE_USUARIO,APELLIDO_USUARIO,FECHA_NACIMIENTO,FOTOPERFIL	 from USUARIO `)
 	for rows.Next() {
 		p := new(USUARIO)
-		rows.Scan(&p.USERNAME, &p.NOMBRE_USUARIO, &p.APELLIDO_USUARIO,&p.APELLIDO_USUARIO)
+		rows.Scan(&p.USERNAME, &p.NOMBRE_USUARIO, &p.APELLIDO_USUARIO,&p.FECHA_NACIMIENTO,&p.FOTOPERFIL)
 		people = append(people, *p)
 	}
 	return people
 }
+func GetDeporte(n int) []Deporte{
+	people := make([]Deporte, 0)
 
+	db, err := sql.Open("godror", "mia1/mia1@localhost:1521/ORCLCDB.localdomain")
+	if err != nil {
+		fmt.Println(err)
+		
+	}
+	defer db.Close()
+	base = db
+	rows, _ := db.Query(`SELECT  DEPORTE,COLOR,FOTO	 from DEPORTE `)
+	for rows.Next() {
+		p := new(Deporte)
+		rows.Scan(&p.Nombre_Deportes,&p.Color_Deporte,&p.Imagen_Deporte )
+		people = append(people, *p)
+	}
+	return people
+}
 
 func main() {
 	//Oracle 12c
@@ -625,23 +854,35 @@ func main() {
 
 	   EN ESTE APARTADO ESTARAN TODAS LAS CONSULTAS QUE  HARAN  LAS CARGAS DE MODELO
 	   **/
-	//peticion carga masiva de los deportes
+	
 	router.HandleFunc("/Login", Login).Methods("POST")
 
 	//peticion carga masiva de todas las temporadas
 	router.HandleFunc("/CargarTemporadas", insert_CargaMasivaTemporada).Methods("POST")
 
+	router.HandleFunc("/CrearDeportes", CrearDeportes).Methods("POST")
+
+	router.HandleFunc("/Usuario/{user}", Perfil).Methods("GET")
+	router.HandleFunc("/Deportes", GetDeportes).Methods("GET")
+
+
+	router.HandleFunc("/EliminarDeportes/{deporte}", EliminarDeportes).Methods("DELETE")
+	router.HandleFunc("/EditarDeportes", EditarDeportes).Methods("POST")
+
+
 	//peticion carga masiva de todas las jornadas
 	router.HandleFunc("/CargarModelo", insert_CargaMasivaModelo).Methods("POST")
+	router.HandleFunc("/Recuperar", Recuperar).Methods("POST")
 
 	//PETICION PARA CREAR USUARIOS
 		router.HandleFunc("/CrearClientes", insert_Clientes).Methods("POST")
-		router.HandleFunc("/Clientes", Perfil).Methods("GET")
+		router.HandleFunc("/EditarClientes", editar_Clientes).Methods("POST")
+	//peticion para obtener el perfil del usuario
+		router.HandleFunc("/Usuario/{user}", Perfil).Methods("GET")
 
-	//peticion para crear deportes
-	router.HandleFunc("/CrearDeportes", CrearDeportes).Methods("POST")
+	//peticion para crear deportesSIMON soo dames).Methods("POST")
 	//peticion para editar deportes
-
+	router.HandleFunc("/files", uploader).Methods("POST")
 	//peticion para eliminar deportes
 	// creando las peticiones que entran  de react
 
